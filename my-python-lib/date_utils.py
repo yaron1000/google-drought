b87cb0766b621735543553aeb9fd7d08e6b9e739
Charles Morton <charles.g.morton@gmail.com>
2014-10-15 11:28:21 -0700

    Simplified set_back_date function using strftime method

--- my-python-lib/date_utils.py
+++ my-python-lib/date_utils.py
@@ -2,24 +2,17 @@
 import datetime
 import re
 
-def set_back_date(days_back):
+def set_back_date(days_back=28):
     '''
     Calculates today - days_back
-    and returns the back date in format
-    yyyymmdd
+    and returns the back date in format yyyymmdd
+    Default is 4 weeks (28 days) back
     '''
     try:
-        int(days_back)
-    except:
+        back_dt = datetime.datetime.today() - datetime.timedelta(days=int(days_back))
+        return back_dt.strftime('%Y%m%d')
+    except ValueError:
         return '99990101'
-    tdy = datetime.datetime.today()
-    #Choose default start_date 4 weeks back
-    b = datetime.datetime.today() - datetime.timedelta(days=int(days_back))
-    yr_b = str(b.year);mon_b = str(b.month);day_b = str(b.day)
-    if len(mon_b) == 1:mon_b = '0%s' % mon_b
-    if len(day_b) == 1:day_b = '0%s' % day_b
-    back_date = '%s-%s-%s' % (yr_b, mon_b, day_b)
-    return back_date
 
 def date_to_datetime(date_str):
     '''
